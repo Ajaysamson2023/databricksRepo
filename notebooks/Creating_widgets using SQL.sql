296fe0b6877ca8b62d3282782a20caa1a995a2b9
Ajay.K <132363430+Ajaysamson2023@users.noreply.github.com>
2023-07-01 19:47:37 +0530

    Worked on Widgets in databricks

--- notebooks/Creating_widgets using SQL.sql
+++ notebooks/Creating_widgets using SQL.sql
@@ -46,3 +46,32 @@ SELECT * FROM persons WHERE gender=getargument('genderDD')
 -- COMMAND ----------
 
 SELECT * from persons WHERE gender=('$genderDD')
+
+-- COMMAND ----------
+
+-- MAGIC %python 
+-- MAGIC data=[(1,'prakash','male'),(2,'ramesh','male'),(3,'arun','female'),(4,'sathya','female'),(5,'mahesh','male'),(6,'Abitha','female'),(7,'pandiyan','male'),(8,'Siva kumar','male')]
+-- MAGIC column=['id','name','gender']
+-- MAGIC df=spark.createDataFrame(data,column)
+-- MAGIC display(df)
+
+-- COMMAND ----------
+
+-- MAGIC %python 
+-- MAGIC df.createOrReplaceTempView('Datas_person')
+
+-- COMMAND ----------
+
+SELECT * FROM Datas_person
+
+-- COMMAND ----------
+
+CREATE WIDGET DROPDOWN genderDD DEFAULT 'male' CHOICES SELECT  gender from Datas_person
+
+-- COMMAND ----------
+
+SELECT * from persons WHERE gender=('$genderDD')
+
+-- COMMAND ----------
+
+SELECT * from persons WHERE gender=('$genderDD')
