18e6b1c044055c0d63ccfdafa2e900065e83ac41
Ajay.K <132363430+Ajaysamson2023@users.noreply.github.com>
2023-07-01 19:58:34 +0530

    Notebook to Passing Values as Parameter to  Another Notebook

--- notebooks/NB_Passing values_from_Another_NB.py
+++ notebooks/NB_Passing values_from_Another_NB.py
@@ -15,3 +15,22 @@ print('ComboBox values is', dbutils.widgets.get('Paramcombobox'))
 print('Dropdown values is', dbutils.widgets.get('Paramdropdown'))
 print('Multiselect values is',dbutils.widgets.get('ParamMultiselect'))
 print('TextBox values is', dbutils.widgets.get('ParamText'))
+
+# COMMAND ----------
+
+dbutils.widgets.combobox(name='Paramcombobox',defaultValue='grapes',choices=['grapes','papaya','Watermelon','mango'],label='Fruits ComboBox')
+dbutils.widgets.dropdown(name='Paramdropdown',defaultValue='grapes',choices=['grapes','papaya','Watermelon','mango'],label='Fruits Dropdown')
+dbutils.widgets.multiselect(name='ParamMultiselect',defaultValue='grapes',choices=['grapes','papaya','Watermelon','mango'],label='Fruits Multiselect')
+dbutils.widgets.text(name='ParamText',defaultValue='grapes',label='Fruits TextBox')
+
+# COMMAND ----------
+
+print('ComboBox values is', dbutils.widgets.get('Paramcombobox'))
+print('Dropdown values is', dbutils.widgets.get('Paramdropdown'))
+print('Multiselect values is',dbutils.widgets.get('ParamMultiselect'))
+print('TextBox values is', dbutils.widgets.get('ParamText'))
+
+# COMMAND ----------
+
+dbutils.secrets.help()
+dbutils.secrets.listScopes()
