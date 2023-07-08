a21e7b8e3473d264ceae25adebf9a198f49091a8
Ajay.K <132363430+Ajaysamson2023@users.noreply.github.com>
2023-07-08 19:05:26 +0530

    Practiced Delta table Changing Dimension Method

--- notebooks/Delta_tables_3.py
+++ notebooks/Delta_tables_3.py
@@ -74,5 +74,53 @@ df.write.option("mergesSchema").insertInto('DBC_College',overwrite = False)
 
 # COMMAND ----------
 
+from delta.tables import *
 
+DeltaTable.create(spark)\
+    .tableName("DBC_College")\
+        .addColumn("class_id","INT")\
+            .addColumn("Name","STRING")\
+                .addColumn("Age","INT")\
+                    .addColumn("Dept","STRING")\
+                        .property("Description","College Students Details")\
+                            .location("/FileStore/shared_uploads/ajay.k@diggibyte.com/delta_tables_3")\
+                                .execute()
  
+
+# COMMAND ----------
+
+# MAGIC  %sql
+# MAGIC select * from DBC_College
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC insert into DBC_College values(400,"Bala",33,"MSC")
+
+# COMMAND ----------
+
+# MAGIC  %sql
+# MAGIC select * from DBC_College
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType,StructField,IntegerType,StringType
+
+student_data=[(500,"Arun",24,"BBA")]
+
+student_schema=StructType([
+    StructField("class_id",IntegerType(),True),\
+    StructField("Name",StringType(),True),\
+    StructField("Age",IntegerType(),True),\
+    StructField("College",StringType(),True)\
+    ])
+df=spark.createDataFrame(data=student_data,schema=student_schema)
+display(df)
+
+# COMMAND ----------
+
+df.write.format("delta").mode("append").saveAsTable("DBC_College")
+
+# COMMAND ----------
+
+display(spark.sql("select * from DBC_College "))
